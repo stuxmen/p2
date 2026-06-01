d1ef8c4e9830639b4fedbca2bd43f15b0fdc1ff5
stuxmen <smithlopez823@gmail.com>
2026-06-01 18:40:07 +0200

    Add files via upload

---
+++ p1.js
@@ -0,0 +1,20 @@
+function busqueda()    {
+        
+        console.log("busqueda");
+    }
+
+function menu()   {
+    
+    window.location.href = "p2.html";
+}
+function sesion() {
+    console.log("inicion de sesion");
+    }
+
+function carrito(){
+    console.log("carrtio echo")
+}
+
+function moneda(){
+    console.log("moneda iniciada")
+}
